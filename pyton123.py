449c05e6ca2aa5c47107ae94b4b3ec3938ffb6b6
Edvin Bergström <edvbe696@li11-6.ida.liu.se>
2017-08-29 17:36:46 +0200

    hejhej

--- pyton123.py
+++ pyton123.py
@@ -1,3 +1,5 @@
+import numbers
+
 print("Start")
 
 
@@ -10,3 +12,56 @@ def max3(a,b,c):
     elif(a<c):
         return c
     else : return a
+
+
+def div_by_three(x):
+    y = x/3
+    print(x ,"divided by 3 equals",y)
+    return  (x%3 == 0)
+
+
+def power(x,y):
+    z = x
+    for i in range(y-1):
+        x*=z
+    return x
+#--------------------------------------------------------------------------------------------
+
+def check_pnr(arr):
+    # print(10-unit(summ(produkt(arr))) ,"funktioner")# print(arr[len(arr)-1] ,"svar")
+    return ((10-unit(summ(produkt(arr))))%10 == arr[len(arr)-1])
+    
+
+
+def summ(arr):
+    tot=0
+    for i in range(len(arr)):
+        tot += arr[i] 
+    return tot
+
+
+def produkt(arr):
+    arr2 = []
+    #print(len(arr))
+    for i in range(len(arr)-1):
+        #print(i)
+        x=arr[i]
+        if (i%2==0):
+            x=x*2
+        arr2.append(unit(x))
+        arr2.append(ten(x))
+    return arr2
+            
+
+
+
+def unit(x):
+    return x%10
+
+
+def ten(x):
+    if (x>9):
+        return (x//10)%10
+    else: return 0
+
+

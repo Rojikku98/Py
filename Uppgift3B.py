8c20599c3f154702c37e5a5e73c07d6162dcf40b
Edvin Bergström <edvbe696@li17-13.ida.liu.se>
2017-09-04 17:04:36 +0200

    klar med 3a

---
+++ Uppgift3B.py
@@ -0,0 +1,17 @@
+print("start")
+
+def choose(k,n):
+    
+    tot =1
+
+
+
+
+def loop(start,end,tot):
+    if start>(end):
+        tot*=start
+        start-=1
+        return loop(start,end,tot)
+    else: return tot
+        
+    XS

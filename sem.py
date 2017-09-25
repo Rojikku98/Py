93650f88e37fe77148004356fed2df62653b138a
Edvin Bergström <edvbe696@parlomba2.ida.liu.se>
2017-09-25 13:48:26 +0200

    hrueh

---
+++ sem.py
@@ -0,0 +1,132 @@
+Python 3.4.3 (default, Nov 17 2016, 01:08:31) 
+[GCC 4.8.4] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> >>> >>> >>> >>> >>> >>> >>> [1, 22, 275, 1]
+>>> [1, 22, 275, 1] hej
+>>> [1, 22, 275, 1] hej
+>>> hej
+>>> hej
+>>> hej
+>>> hej [97, 115, 107, 100, 100]
+>>> hej [97, 115, 107, 100, 100] a
+>>> hej [97, 115, 107, 100, 100] a
+>>> hej ['a', 's', 'k', 'd', 'd'] a
+>>> hej ['a', 's', 'k', 'd', 'd'] a
+>>> hej ['a', 's', 'k', 'd', 'd'] a
+>>> hej [97, 115, 107, 100, 100] a
+>>> hej [65, 115, 107, 100, 100] A
+>>> hej [65, 115, 107, 100, 100] A
+[65, 115, 107, 100, 100]
+>>> hej [65, 115, 107, 100, 100] A
+[65, 115, 107, 100, 100]
+>>> [65, 115, 107, 100, 100]
+>>> [65, 115, 107, 100, 100]
+>>> Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "/home/edvbe696/Documents/sem4.py", line 2, in <module>
+  File "/home/edvbe696/Documents/sem4.py", line 10, in main2
+  File "/home/edvbe696/Documents/sem4.py", line 10, in <listcomp>
+TypeError: ord() expected string of length 1, but int found
+>>> Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "/home/edvbe696/Documents/sem4.py", line 2, in <module>
+NameError: name 'main3' is not defined
+>>> Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "/home/edvbe696/Documents/sem4.py", line 2, in <module>
+NameError: name 'main3' is not defined
+>>> Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "/home/edvbe696/Documents/sem4.py", line 2, in <module>
+NameError: name 'main3' is not defined
+>>> Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "/home/edvbe696/Documents/sem4.py", line 2, in <module>
+NameError: name 'main3' is not defined
+>>> Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "/home/edvbe696/Documents/sem4.py", line 2, in <module>
+    print(main3([[1,2,3],[3,23,7],[1,45,7,9]]))
+NameError: name 'main3' is not defined
+>>> s
+>>> s
+[[1, 2, 3], [3, 23, 7], [1, 45, 7, 9]]
+None
+>>> s
+[[1, 2, 3], [3, 23, 7], [1, 45, 7, 9]]
+None
+>>> s
+[[1, 2, 3], [3, 23, 7], [1, 45, 7, 9]]
+None
+>>> s
+[[1, 2, 3], [3, 23, 7], [1, 45, 7, 9]] hej
+None
+>>> s
+[[1, 2, 3], [3, 23, 7], [1, 45, 7, 9]] hej
+None
+>>> s
+[[1, 2, 3], [3, 23, 7], [1, 45, 7, 9]]
+None
+>>> s
+[[1, 2, 3], [3, 23, 7], [1, 45, 7, 9]]
+None
+>>> s
+[[1, 2, 3], [3, 23, 7], [1, 45, 7, 9]]
+None
+>>> s
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "/home/edvbe696/Documents/sem4.py", line 2, in <module>
+    print(main3([[1,2,3],[3,23,7],[1,45,7,9]]))
+  File "/home/edvbe696/Documents/sem4.py", line 16, in main3
+    print(res)
+NameError: name 'j' is not defined
+>>> s
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "/home/edvbe696/Documents/sem4.py", line 2, in <module>
+    print(main3([[1,2,3],[3,23,7],[1,45,7,9]]))
+  File "/home/edvbe696/Documents/sem4.py", line 16, in main3
+    print(res)
+NameError: name 'j' is not defined
+>>> s
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "/home/edvbe696/Documents/sem4.py", line 2, in <module>
+    print(main3([[1,2,3],[3,23,7],[1,45,7,9]]))
+  File "/home/edvbe696/Documents/sem4.py", line 16, in main3
+    print(res)
+NameError: name 'j' is not defined
+>>> s
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "/home/edvbe696/Documents/sem4.py", line 2, in <module>
+    print(main3([[1,2,3],[3,23,7],[1,45,7,9]]))
+  File "/home/edvbe696/Documents/sem4.py", line 16, in main3
+    print(res)
+NameError: name 'j' is not defined
+>>> s
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "/home/edvbe696/Documents/sem4.py", line 2, in <module>
+    print(main3([[1,2,3],[3,23,7],[1,45,7,9]]))
+  File "/home/edvbe696/Documents/sem4.py", line 16, in main3
+    print(res)
+NameError: name 'j' is not defined
+>>> s
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "/home/edvbe696/Documents/sem4.py", line 2, in <module>
+    print(main3([[1,2,3],[3,23,7],[1,45,7,9]]))
+  File "/home/edvbe696/Documents/sem4.py", line 16, in main3
+    print(res)
+NameError: name 'j' is not defined
+>>> s
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "/home/edvbe696/Documents/sem4.py", line 2, in <module>
+    print(main3([[1,2,3],[3,23,7],[1,45,7,9]]))
+  File "/home/edvbe696/Documents/sem4.py", line 16, in main3
+    print(res)
+NameError: name 'j' is not defined
+>>> 
